ab8616d1d7b38e6dad674b0f16399ce505b36970
Jean <jean.carlosmolossi@gmail.com>
2022-03-20 22:58:24 -0300

    adicionado env config

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,11 +1,13 @@
+import "dotenv/config";
+
 export default {
 	name: "default",
 	type: "mysql",
-	host: "todo_db",
-	port: 3306,
-	username: "todo",
-	password: "todo",
-	database: "todos",
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT ?? 3306,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_BASE,
 	synchronize: true,
 	// logging: true,
 	entities: ["src/main/entities/**/*.ts", "src/data/**/*.entity.ts"],
